04081346aa96a6bfe110d338c4a507c22459e942
matsuo <matsuo.tomoki@pilot-inc.co.jp>
2020-07-17 15:38:05 +0900

    GetFileListのif文修正

--- ExtensionChange/ExtensionChangeDlg.cpp
+++ ExtensionChange/ExtensionChangeDlg.cpp
@@ -390,7 +390,7 @@ BOOL CExtensionChangeDlg::GetFileList(CString path, bool flag)
 			else if (m_text_previousExtension == L"" && fileName.ReverseFind(L'.') != -1) { //変換前拡張子がなし、判定したいファイルの拡張子はあり
 				continue;
 			}
-			else if (m_text_previousExtension != L"" && fileExtension != m_text_previousExtension) { //変換前拡張子があり、変換前拡張子と実際の拡張子が異なる
+			else if (m_text_previousExtension != L"" &&  m_text_previousExtension != fileExtension) { //変換前拡張子があり、変換前拡張子と実際の拡張子が異なる
 				continue;
 			}
 			else {
